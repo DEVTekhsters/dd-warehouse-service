bdbb9166a16f619aeaff5632e350d38cf6fada03
HY-AV <hemlata@appvintech.com>
2024-11-26 16:24:49 +0530

    updated ip

--- client_connect.py
+++ client_connect.py
@@ -1,11 +1,11 @@
 # ITs only for  tesing use of the connection 
 
 import clickhouse_connect
-
+                            
 class Connection:
    
     client = clickhouse_connect.get_client(
-            host='13.202.114.233',
+            host='148.113.6.50',
             port="8123",
             username='default',
             password='',
