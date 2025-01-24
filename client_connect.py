06538c0c02ea121e3039d812fa19d5bbc1b3ce82
HY-AV <hemlata@appvintech.com>
2025-01-24 10:48:47 +0530

    updated .env

--- client_connect.py
+++ client_connect.py
@@ -1,14 +1,17 @@
-# ITs only for  tesing use of the connection 
+# ITs only for testing use of the connection
 
 import clickhouse_connect
-                            
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
 class Connection:
-   
     client = clickhouse_connect.get_client(
-            host='148.113.6.50',
-            port="8123",
-            username='default',
-            password='',
-            database='default'
-        )
-    
+        host=os.getenv('CLICKHOUSE_HOST'),
+        port=os.getenv('CLICKHOUSE_PORT'),
+        username=os.getenv('CLICKHOUSE_USERNAME'),
+        password=os.getenv('CLICKHOUSE_PASSWORD'),
+        database=os.getenv('CLICKHOUSE_DATABASE')
+    )
