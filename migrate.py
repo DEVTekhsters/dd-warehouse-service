bf932ce2a606f55babe5ac09baf9e4ac7b158ca5
HY-AV <hemlata@appvintech.com>
2025-01-27 10:56:45 +0530

    minor

--- migrate.py
+++ migrate.py
@@ -1,8 +1,47 @@
 import os
-from client_connect import Connection
+import clickhouse_connect
+from dotenv import load_dotenv
 
+
+
+load_dotenv()
 # Connect to ClickHouse
-client = Connection.client
+client = clickhouse_connect.get_client(
+        host=os.getenv('CLICKHOUSE_HOST'),
+        port=os.getenv('CLICKHOUSE_PORT'),
+        username=os.getenv('CLICKHOUSE_USERNAME'),
+        password=os.getenv('CLICKHOUSE_PASSWORD'),
+        database=os.getenv('CLICKHOUSE_DATABASE')
+    )
+# -------------------------------------------------------------------------------------
+data = {
+    "Personal_Identification": [
+        "AADHAAR", "PASSPORT", "DRIVERLICENSE", "VOTERID", "SSN", "UID", "PAN", "VID", "RATION_CARD_NUMBER"
+    ],
+    "Contact_Information": [
+        "ADDRESS", "EMAIL_ADDRESS", "PHONE_NUMBER", "POBOX", "ZIPCODE", "LOCATION"
+    ],
+    "Biometric Data": [
+        "BIOMETRIC", "IMEI", "IMSI"
+    ],
+    "Financial_Information": [
+        "BANK_ACCOUNT_NUMBER", "BANK_CARD", "CREDIT_CARD", "AMEX_CARD", "MAESTRO_CARD", "RUPAY_CARD", 
+        "VISA_CARD", "MASTER_CARD", "CVV", "PAYMENTCARD", "UPI_ID", "GST_NUMBER", "IFSC"
+    ],
+    "System_Data": [
+        "IP_ADDRESS", "MAC_ADDRESS", "DATE_TIME", "USERNAME", "USER_NAME", "PASSWORD"
+    ],
+    "Demographic_Information": [
+        "GENDER", "NATIONALITY", "RELIGION", "POLITICAL_OPINION", "SEXUAL_ORIENTATION", "TITLE", "PERSON"
+    ],
+    "Health": [
+        "MEDICAL_LICENSE"
+    ],
+    "Vehicle_Information": [
+        "VEHICLE_IDENTIFICATION_NUMBER"
+    ]
+}
+# -------------------------------------------------------------------------------------
 
 # Directory containing migration files
 migration_dir = './migrations'
@@ -32,6 +71,26 @@ for migration in migrations:
 
 print("All migrations applied.")
 
+# Add data elements to the table `data_element`
+id_counter = 1
+data_uppercase = {key.upper(): [value.upper() for value in values] for key, values in data.items()}
+
+for category, values in data_uppercase.items():
+    parameter_name = category
+    parameter_value = values  # Keep it as a Python list
+
+    # Insert into ClickHouse
+    insert_query = f"""
+    INSERT INTO data_element (id, parameter_name, parameter_value)
+    VALUES ({id_counter}, '{parameter_name}', {parameter_value})
+    """
+    client.command(insert_query)
+    print(f"Inserted {parameter_name} with ID {id_counter}")
+
+    id_counter += 1
+
+
+print("Data insertion complete.")
 # Apply views from structured and unstructured 
 
 # Directories containing view definitions
--- views/unstructured/unstructured_view_data_sensitivity.sql
+++ views/unstructured/unstructured_view_data_sensitivity.sql
@@ -1,7 +1,7 @@
 CREATE VIEW IF NOT EXISTS unstructured_view_data_sensitivity AS 
 SELECT 
-    data_sensitivity,
-    COUNT(data_sensitivity)
+    data_sensitivity as sensitivity,
+    COUNT(data_sensitivity) sensitivity_count
 FROM 
     ner_unstructured_data
 GROUP BY 
