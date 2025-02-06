92c7fa66c0ca9082815e71d3b7eb5c5b319a98d3
HY-AV <hemlata@appvintech.com>
2025-02-06 12:00:35 +0530

    updated view DL

--- views/unstructured/unstructured_view_document_listing.sql
+++ views/unstructured/unstructured_view_document_listing.sql
@@ -2,7 +2,7 @@ CREATE VIEW IF NOT EXISTS unstructured_view_document_listing AS
 SELECT
     file_name AS document_name,                     
     file_type AS document_type,                  
-    detected_entity AS entity,
+    JSONExtract(json, 'detected_entities', 'String') AS entity,  -- Specify String as the return type for detected_entities
     file_size / 1024 AS document_size_kb,  
     region AS location,                            
     source AS data_source,
