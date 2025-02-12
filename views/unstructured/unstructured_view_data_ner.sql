00513e1f53412fe6bbc6fb4add316adab0c71a87
HY-AV <hemlata@appvintech.com>
2025-02-12 15:56:29 +0530

    updated image logic in instant classifier

---
+++ views/unstructured/unstructured_view_data_ner.sql
@@ -0,0 +1,9 @@
+CREATE VIEW IF NOT EXISTS unstructured_view_data_ner AS 
+SELECT 
+    c.*, 
+    d.parameter_sensitivity
+FROM 
+    ner_unstructured_data c
+LEFT JOIN 
+    data_element d
+    ON c.detected_entity = d.parameter_value;
