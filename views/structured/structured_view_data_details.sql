981dd6f105c06ba185ff50181d0a60ed63978291
HY-AV <hemlata@appvintech.com>
2025-02-17 16:08:26 +0530

    Updated structured_view_data_details.sql

--- views/structured/structured_view_data_details.sql
+++ views/structured/structured_view_data_details.sql
@@ -1,6 +1,6 @@
 CREATE VIEW IF NOT EXISTS structured_view_data_details AS 
 SELECT 
-    COUNT(c.id) AS total_data_elements,  -- Count of all data elements
+    (SELECT COUNT(*) FROM column_ner_results) AS total_data_elements,  -- Count of all data elements
     COUNT(DISTINCT c.detected_entity) AS total_data_element_type,  -- Count of distinct detected entity types
     (SELECT COUNT(DISTINCT parameter_name) FROM data_element) AS total_data_element_category  -- Count all distinct parameter names in data_element
 FROM 
